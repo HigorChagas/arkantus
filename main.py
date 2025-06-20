b7a394b7870bfcfe3fe134cf1a8e8ea55774bef4
Higor Chagas <higornchagas@gmail.com>
2025-06-19 23:23:44 -0300

    refactor: add main function and clean up print statements

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
-from dotenv import load_dotenv
 from google import genai
+from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=["Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."]
+    )
 
-client = genai.Client(api_key=api_key)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: ")
+    print(response.text)
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=["Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."]
-)
-
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
