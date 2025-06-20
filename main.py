4d92b113df3e87b845bb18155afadac64f3bc112
Higor Chagas <higornchagas@gmail.com>
2025-06-20 10:49:06 -0300

    feat: allow prompt input via CLI args with fallback if missing

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
 
+
 def main():
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    del sys.argv[0]
+    user_prompt = " ".join(sys.argv)
+
+    print(user_prompt)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=["Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."]
+        contents=[user_prompt]
     )
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: ")
+    print(f"Sistem Args: {sys.argv}")
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
